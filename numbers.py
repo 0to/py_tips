a71e8e68a30b7a4a93fb33eaa3bce15741f9f65e
weilong.wang@memblaze.com <weilong.wang@memblze.com>
2020-03-12 21:50:08 +0800

    add numbers.py

---
+++ numbers.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'''
+https://www.python.org/dev/peps/pep-0515/
+Python-Version:	3.6
+为了方便阅读，较大的数字可以使用下划线进行连接。
+'''
+assets0 = 100_000_000
+assets1 = int('100_000_000')
+print(assets0, assets1)
