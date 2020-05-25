8a7587999707feec393055f0107cf6a8958d2076
weilong.wang@memblaze.com <weilong.wang@memblze.com>
2020-05-25 12:14:46 +0800

    update python3 round tips

--- numbers.py
+++ numbers.py
@@ -9,3 +9,16 @@ Python-Version:	3.6
 assets0 = 100_000_000
 assets1 = int('100_000_000')
 print(assets0, assets1)
+
+
+'''
+Rounding seems easy, round(1.1) evaluates to 1, round(1.8) evaluates to 2.
+The question is, how do we round the .5 numbers?
+Should you round up? down? Turns out, there are a lot of ways to do it..
+Python 3 uses bankers rounding. Odd numbers are rounded up, even numbers are rounded down.
+https://en.wikipedia.org/wiki/Rounding#Round_half_to_even
+The reasoning behind this method is that if you round a list of numbers, assuming there’s roughly the same number of odd and even numbers, the error (rounding) will cancel each other.
+'''
+print(round(1.5), round(2.5))  # This code will print: 2 2
+print(round(1.6), round(2.6))  # This code will print: 2 3
+print(round(1.4), round(2.4))  # This code will print: 1 2
